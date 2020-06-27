161703c2f290b0ff4958446cf1fb4bc2db1fad4d
Paul Joachim <PaulDJoachim@gmail.com>
2020-06-26 21:41:10 -0500

    added storage and handlers for operators

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,30 @@
 console.log('javascript is working!');
 
+//// DECLARATIONS ////
+
+// storage for last operator clicked
+let opStorage
+
+
+//// THE READY-NOW THING ////
+
 $(readyNow);
+
+
+//// FUNCTIONS ////
+
 
 function readyNow() {
     console.log('jQuery is working!');
-    
+    $('.opBtn').on('click', opStore);
+    $('#calculateBtn').on('click', sendInput)
 }
+
+
+function opStore() {
+    console.log('operator button clicked:', $(this)[0].innerText);
+    opStorage = $(this)[0].innerText;
+    console.log('opStorage now holds:', opStorage);
+}
+
+

e7e136806d6faa2e502b33a7eaab5ae1834a116f
Paul Joachim <PaulDJoachim@gmail.com>
2020-06-27 21:50:48 -0500

    math works again, added clear functions

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,8 +3,8 @@ console.log('javascript is working!');
 //// DECLARATIONS ////
 
 // count how many operators have been added
-opPresent = false;
-
+opAdded = false;
+dotCount = 0;
 
 //// THE READY-NOW THING ////
 
@@ -16,26 +16,56 @@ $(readyNow);
 
 function readyNow() {
     console.log('jQuery is working!');
-    //getHistory()
+    getHistory()
     $('.opBtn').on('click', opCop);
+    $('.dotBtn').on('click', dotCop);
     $('#calculateBtn').on('click', sendInput);
+    $('#clearBtn').on('click', clearDisplay);
 }
 
 
 // opCop polices the user's input of operators
 function opCop() {
-    console.log('an operator button was clicked');
+    dotCount = 0;
     // if a second operator is clicked remove it from the display
-    if (opPresent) {
+    if (opAdded) {
         $('#display').val($('#display').val().slice(0,-1));
+        console.log('OP COP SAYS: ONLY 1 OPERATOR ALLOWED!');
+        return;
         //ADD A USER NOTIFICATION THAT THEY CAN ONLY DO 1 OPERATOR!
+    } else if ($('#display').val().length === 1) {
+        $('#display').val($('#display').val().slice(0,-1));
+        console.log('OP COP SAYS: YOU CAN\'T START WITH THE OPERATOR!')
+        return;
     }
-    opPresent = true;
+    opAdded = true;
 }
 
 
+// dot cop makes sure only one decimal is added to either side of the equation
+function dotCop() {
+    if (dotCount === 1 && !opAdded) {
+        $('#display').val($('#display').val().slice(0,-1));
+        console.log('DOT COP SAYS: NO MORE DOTS UNTIL YOU ADD AN OPERATOR!!')
+        return;
+    } else if (dotCount === 1 && opAdded){
+        $('#display').val($('#display').val().slice(0,-1));
+        console.log('DOT COP SAYS: NO MORE DOTS FOR YOU!!!')
+        return;
+    }
+    dotCount++
+}
+
+
+// makes an inputPacket out of the input, sends to server, gets answer object back.
 function sendInput() {
     const inputPacket = inputParser($('#display').val());
+    // check to make sure all the inputs actually have data before sending
+    if (!syntaxCheck(inputPacket)) {
+        console.log('THE INPUT IS MISSING SOMETHING!')
+        //ADD MESSAGE TO USER ABOUT BAD SYNTAX
+        return false;
+    }
     console.log('Attempting to POST @ /calculate:',inputPacket);
     $.ajax({
         type: 'POST',
@@ -44,15 +74,15 @@ function sendInput() {
     }).then (function(response){
         console.log('reply from /calculate POST is:', response);
         // put a get request here for the answer?
-        //getHistory()
+        getHistory()
     }).catch (function(error){
         alert('Sorry, something went wrong!');
         console.log('Error on /calculate POST to server:', error);
     })
-
 }
 
 
+// sends a GET request to the server for all previous inputs & results
 function getHistory() {
     $.ajax({
         type: 'GET',
@@ -66,10 +96,11 @@ function getHistory() {
 }
 
 
+// print all results on the dom
 function printEverything(array) {
     console.log('later I will print this on the DOM:', array);
     $('#outputArea').children().empty();
-    $('#resultDisplay').append(array[array.length-1].result);
+    $('#result').val(array[array.length-1].result);
     for (let i=array.length-1; i>=0; i--) {
         $('#resultList').append(`
         <li>${array[i].input1} ${array[i].operator} ${array[i].input2} = ${array[i].result}</li>`);
@@ -77,17 +108,21 @@ function printEverything(array) {
 }
 
 
+// take the calculator display and turn it into an object for sending to server 
 function inputParser(string) {
     let input1 = '';
     let input2 = '';
     let operator = '';
     let onFirst = true;
     for (let i=0; i<string.length; i++){
-        if (Number.isInteger(Number(string[i])) && onFirst) {
+        // separate input1
+        if (Number.isInteger(Number(string[i])) && onFirst || string[i] === '.') {
             input1 += string.substring(i,i+1);
+        // separate operator
         } else if (onFirst) {
             operator += string.substring(i,i+1)
             onFirst = false;
+        // whatever is left in the string add to input2
         } else {
             input2 += string.substring(i);
             break;
@@ -101,4 +136,20 @@ function inputParser(string) {
 }
 
 
-console.log(inputParser('122*500'));
+//checks the outgoing packet to make sure all parameters have values
+function syntaxCheck(object) {
+    if (object.input1 === '' || object.input2 === '' || object.operator === '') {
+        return false
+    }
+    return true
+}
+
+
+// clears the display, resets the decimal and operator tracking
+function clearDisplay() {
+    console.log('reset pressed');
+    $('#result').val('');
+    $('#display').val('');
+    opAdded = false;
+    dotCount = 0;
+}
